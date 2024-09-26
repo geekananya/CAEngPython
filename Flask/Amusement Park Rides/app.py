e6bccc292694f9715d20987aa8e0b32352fa0421
Ananya Verma <ananyaverma@Ananyas-MacBook-Pro.local>
2024-09-26 10:46:41 +0530

    add authentication route and validation module

--- Flask/Amusement Park Rides/app.py
+++ Flask/Amusement Park Rides/app.py
@@ -1,43 +1,44 @@
-from flask import Flask, render_template, request
+from database import credentials, rides
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-rides = [
-    {"name": "Roller Coaster", "age_limit": 12, "height_limit": 140, "price": 10},
-    {"name": "Ferris Wheel", "age_limit": 0, "height_limit": 0, "price": 5},
-    {"name": "Bumper Cars", "age_limit": 8, "height_limit": 120, "price": 7},
-    {"name": "Haunted House", "age_limit": 10, "height_limit": 0, "price": 8},
-    {"name": "Merry-Go-Round", "age_limit": 0, "height_limit": 0, "price": 4},
-    {"name": "Water Slide", "age_limit": 5, "height_limit": 100, "price": 6},
-    {"name": "Log Flume", "age_limit": 6, "height_limit": 110, "price": 7},
-    {"name": "Swing Ride", "age_limit": 0, "height_limit": 0, "price": 5},
-    {"name": "Drop Tower", "age_limit": 12, "height_limit": 130, "price": 9},
-    {"name": "Go-Karts", "age_limit": 10, "height_limit": 130, "price": 15},
-]
-
 def check_credentials(user, passw):
-    from database import credentials
+    # from database import credentials
     for cred in credentials:
-        if cred.username == user and cred.password == passw:
-            if cred.username == 'admin':
+        # print(cred)
+        if cred['username'] == user and cred['password'] == passw:
+            if cred['username'] == 'admin':
                 return 'admin'
             else:
                 return 'user'
     return False
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
+def index():
+    print('on root route')
+    return redirect('/login', 302)
+
+@app.route('/login', methods=['GET', 'POST'])       # to tell which methods this route will respond to (by default, only GET)
 def login():
     error = None
     if request.method == 'POST':
-        res = check_credentials(request.form['username'], request.form['password'])
+        res = check_credentials(request.form.get('username'), request.form.get('password'))
         if res:
-            return render_template(index.html)
+            return redirect(f'/home/{res}')
         else:
             error = 'Invalid username/password'
+
     return render_template('login.html', error=error)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/home/<usertype>', methods=['GET', 'POST'])
+def home(usertype):
+
+    # if usertype == 'admin':
+    #     if request.method == 'POST':
+    #
+    #     return render_template('/admin_dashboard.html')
+
     if request.method == 'POST':
         age = int(request.form['age'])
         height = int(request.form['height'])
@@ -53,7 +54,7 @@ def index():
 
         return render_template('result.html', available_rides=available_rides, total_price=total_price, age=age, height=height)
 
-    return render_template('index.html',rides=rides)
+    return render_template('index.html',rides=rides, usertype=usertype)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Flask/Amusement Park Rides/database.py
+++ Flask/Amusement Park Rides/database.py
@@ -1,4 +1,18 @@
 credentials = [
     {"username": "admin", "password": "adminpass"},
-    {"username": "user", "password": "userpass"},
+    {"username": "user1", "password": "userpass1"},
+    {"username": "user2", "password": "userpass2"},
+]
+
+rides = [
+    {"name": "Roller Coaster", "age_limit": 12, "height_limit": 140, "price": 10},
+    {"name": "Ferris Wheel", "age_limit": 0, "height_limit": 0, "price": 5},
+    {"name": "Bumper Cars", "age_limit": 8, "height_limit": 120, "price": 7},
+    {"name": "Haunted House", "age_limit": 10, "height_limit": 0, "price": 8},
+    {"name": "Merry-Go-Round", "age_limit": 0, "height_limit": 0, "price": 4},
+    {"name": "Water Slide", "age_limit": 5, "height_limit": 100, "price": 6},
+    {"name": "Log Flume", "age_limit": 6, "height_limit": 110, "price": 7},
+    {"name": "Swing Ride", "age_limit": 0, "height_limit": 0, "price": 5},
+    {"name": "Drop Tower", "age_limit": 12, "height_limit": 130, "price": 9},
+    {"name": "Go-Karts", "age_limit": 10, "height_limit": 130, "price": 15},
 ]
