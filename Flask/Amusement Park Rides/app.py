38be1bc5b937dc9bb2c8741faecec0f20144eb20
Ananya Verma <ananyaverma@Ananyas-MacBook-Pro.local>
2024-09-27 02:25:00 +0530

    add crud ops for admin, refactor dummy database into class

--- Flask/Amusement Park Rides/app.py
+++ Flask/Amusement Park Rides/app.py
@@ -1,7 +1,12 @@
-from database import credentials, rides
-from flask import Flask, render_template, request, redirect
+from xml.dom import NotFoundErr
+
+from database import credentials, Rides, rides_data
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+ridesInstance = Rides()     #instantiate class for this session
+ridesInstance.initialize_rides(rides_data)
+
 
 def check_credentials(user, passw):
     # from database import credentials
@@ -33,11 +38,10 @@ def login():
 
 @app.route('/home/<usertype>', methods=['GET', 'POST'])
 def home(usertype):
-
-    # if usertype == 'admin':
-    #     if request.method == 'POST':
-    #
-    #     return render_template('/admin_dashboard.html')
+    if usertype == 'admin':
+        if request.method == 'POST':
+            print('post')
+        return render_template('/admin_dashboard.html', rides = ridesInstance.get_rides())
 
     if request.method == 'POST':
         age = int(request.form['age'])
@@ -47,14 +51,62 @@ def home(usertype):
         available_rides = []
         total_price = 0
 
-        for ride in rides:
-            if age >= ride["age_limit"] and (ride["height_limit"] == 0 or height >= ride["height_limit"]):
+        for ride in ridesInstance.get_rides():
+            if age >= ride.age_limit and (ride.height_limit == 0 or height >= ride.height_limit):
                 available_rides.append(ride)
-                total_price += ride["price"]
+                total_price += ride.price
 
         return render_template('result.html', available_rides=available_rides, total_price=total_price, age=age, height=height)
 
-    return render_template('index.html',rides=rides, usertype=usertype)
+    return render_template('index.html',rides=ridesInstance.get_rides(), usertype=usertype)
+
+
+@app.route('/rides/add-ride', methods=['GET', 'POST'])
+def add_ride():             # url-for looks for the function that handles itm and not the route.
+    if request.method == 'POST':
+        name = request.form.get('name')
+        age_limit = int(request.form['age_limit'])
+        height_limit = int(request.form['height_limit'])
+        price = int(request.form.get('price'))
+
+        ridesInstance.add_ride(name=name, age_limit=age_limit, height_limit=height_limit, price=price)
+
+        print("added successfully")
+        return redirect('/home/admin')
+
+    return render_template('newride.html')
+
+@app.route('/rides/remove-ride')
+def remove_ride():
+    try:
+        ridesInstance.remove_ride(request.args.get('ride'))
+    except NotFoundErr as nfe:
+        print(nfe)
+    finally:
+        return redirect(url_for('home', usertype='admin'))
+        # return render_template('admin_dashboard.html', rides = ridesInstance.get_rides())   # cant use redirect here because we are inside rides/
+
+@app.route('/rides/update-ride')
+def update_ride():
+    name = request.args.get('ride')
+    print(name)
+    if request.method == 'POST':
+        try:
+            info = {}
+            if request.form.get('age_limit') is not None:
+                info['age_limit'] = request.form.get('age_limit')
+            if request.form.get('height_limit') is not None:
+                info['height_limit'] = request.form.get('height_limit')
+            if request.form.get('price') is not None:
+                info['price'] = request.form.get('price')
+            ridesInstance.add_ride(name, info)
+
+        except Exception as e:
+            print(e)
+        finally:
+            return redirect('/home/admin')
+    return render_template('editride.html', ride=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- Flask/Amusement Park Rides/database.py
+++ Flask/Amusement Park Rides/database.py
@@ -1,10 +1,70 @@
+class Ride:
+    def __init__(self, name, age_limit, height_limit, price):
+        self.name = name
+        self.age_limit = age_limit
+        self.height_limit = height_limit
+        self.price = price
+
+    def __repr__(self):
+        return f"Ride(name='{self.name}', age_limit={self.age_limit}, height_limit={self.height_limit}, price={self.price})"
+
+    # def __str__(self):
+    #     return f"Ride(name='{self.name}', age_limit={self.age_limit}, height_limit={self.height_limit}, price={self.price})"
+
+
+# class to handle add/remove ops on ride
+class Rides:
+    def __init__(self):
+        self.rides_list = []        # compositely stores Ride instances
+
+    def initialize_rides(self, data):
+        for ride_data in data:
+            self.add_ride(
+                ride_data["name"],
+                ride_data["age_limit"],
+                ride_data["height_limit"],
+                ride_data["price"]
+            )
+
+    def add_ride(self, name, age_limit, height_limit, price):
+        new_ride = Ride(name, age_limit, height_limit, price)
+        self.rides_list.append(new_ride)
+
+
+    def remove_ride(self, name):
+        for ride in self.rides_list:
+            if ride.name == name:
+                self.rides_list.remove(ride)
+                return self.get_rides()
+        raise Exception("Ride not found.")
+
+
+    def update_ride(self, name, **ride_info):
+        for ride in self.rides_list:
+            if ride.name == name:
+                if ride_info.get('age_limit') is not None:
+                    ride.age_limit = ride_info.get('age_limit')
+                if ride_info.get('height_limit') is not None:
+                    ride.height_limit = ride_info.get('height_limit')
+                if ride_info.get('price') is not None:
+                    ride.price = ride_info.get('price')
+                return self.get_rides()
+        raise Exception("Ride not found.")
+
+
+    def get_rides(self):
+        if not self.rides_list:
+            return []
+        return self.rides_list
+
+
 credentials = [
     {"username": "admin", "password": "adminpass"},
     {"username": "user1", "password": "userpass1"},
     {"username": "user2", "password": "userpass2"},
 ]
 
-rides = [
+rides_data = [
     {"name": "Roller Coaster", "age_limit": 12, "height_limit": 140, "price": 10},
     {"name": "Ferris Wheel", "age_limit": 0, "height_limit": 0, "price": 5},
     {"name": "Bumper Cars", "age_limit": 8, "height_limit": 120, "price": 7},
